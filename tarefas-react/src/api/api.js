2d249865673cd1c450dafaa81fb8fa34e70e6646
FredyyLima <fredyy_lima@live.com>
2021-11-24 19:37:53 -0300

    atualização para conexão externa

--- tarefas-react/src/api/api.js
+++ tarefas-react/src/api/api.js
@@ -1,5 +1,5 @@
 const Api = {
-  apiUrl: 'http://localhost:3001/tarefas',
+  apiUrl: 'https://to-do-list--backend.herokuapp.com/tarefas',
   fetchGetAll: () => fetch(Api.apiUrl),
   fetchGetById: (id) => fetch(`${Api.apiUrl}/${id}`),
   fetchPost: (data) => {
